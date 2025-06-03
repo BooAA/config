39dc1caa96e858f6ea118990c73d22ebed54114e
BooAA <s930054123yaoyao@gmail.com>
2025-06-03 11:20:58 +0800

    update

--- .emacs.d/exwm/profile/a/init.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-xrandr --newmode profile_a 60 3840 0 0 0 2400 0 0 0
-xrandr --addmode DUMMY0 profile_a
--- .emacs.d/exwm/profile/b/init.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-xrandr --newmode profile_b 60 3840 0 0 0 2160 0 0 0
-xrandr --addmode DUMMY0 profile_b
